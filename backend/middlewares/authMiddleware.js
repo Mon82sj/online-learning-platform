f5e5380e8eb9c7f390d555054179779526fc1b86
Mon82sj <115721479+Mon82sj@users.noreply.github.com>
2024-11-12 22:26:17 +0530

    Update authMiddleware.js

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -10,7 +10,7 @@ module.exports = async (req, res, next) => {
     }
 
     const token = req.headers["authorization"].split(" ")[1];
-    jwt.verify(token, 'MOWNICA@2021#dk', (err, decode) => {
+    jwt.verify(token, process.env.jwt_token, (err, decode) => {
       if (err) {
         return res
           .status(200)
